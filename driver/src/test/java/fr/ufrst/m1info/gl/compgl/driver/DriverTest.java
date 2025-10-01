387b8655143345633f82ed74346896d87ef1bf9a
QuentinRdl <quentinradlo.pro@gmail.com>
2025-10-01 13:00:56 +0200

    [ADD] - Added a base test file for the driver module, and updated the gitignore

---
+++ driver/src/test/java/fr/ufrst/m1info/gl/compgl/driver/DriverTest.java
@@ -0,0 +1,13 @@
+package fr.ufrst.m1info.gl.compgl.driver;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DriverTest {
+
+    @Test
+    void dummyTest() {
+        // Testing that this test class is runned
+        assertTrue(true, "Should be running and passing !");
+    }
+}
