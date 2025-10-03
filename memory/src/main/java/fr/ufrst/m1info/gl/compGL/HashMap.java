b9d6ee9f794e223a60a15056db3e6b8e3682d385
theo humbert <theo.humbert06@gmail.com>
2025-10-03 16:09:22 +0200

    [ADD] HashMap

---
+++ memory/src/main/java/fr/ufrst/m1info/gl/compGL/HashMap.java
@@ -0,0 +1,153 @@
+package fr.ufrst.m1info.gl.compGL;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class HashMap<K,V> extends AbstractMap<K,V> implements Map<K,V>, Cloneable, Serializable
+{
+    private int capacity;
+    private float loadFactor;
+    private List<EntryHashMap<K,V>>[] buckets;
+    private int sizeHashMap;
+
+    /**
+     * Constructs an empty HashMap with the specified initial capacity and load factor.
+     *
+     * @param initialCapacity the initial capacity
+     * @param loadFactor the load factor
+     */
+    public HashMap(int initialCapacity, float loadFactor){
+        if (initialCapacity<0 && loadFactor<=0.0){
+            throw new IllegalArgumentException("Arguments cannot be negative");
+        }
+        capacity=initialCapacity;
+        this.loadFactor=loadFactor;
+        buckets=new List[capacity];
+        for (int i=0;i<capacity;i++){
+            buckets[i]=new ArrayList<>();
+        }
+        sizeHashMap=0;
+    }
+
+    /**
+     * Constructs an empty HashMap with the specified initial capacity and the default load factor (0.75).
+     *
+     * @param initialCapacity the initial capacity.
+     */
+    public HashMap(int initialCapacity){
+        this(initialCapacity,0.75F);
+    }
+
+    /**
+     * Constructs an empty HashMap with the default initial capacity (16) and the default load factor (0.75)
+     */
+    public HashMap(){
+        this(16,0.75F);
+    }
+
+    /**
+     * TODO : Constructs a new HashMap with the same mappings as the specified Map.
+     * The HashMap is created with default load factor (0.75) and an initial capacity sufficient to hold the mappings in the specified Map.
+     *
+     * @param m the map whose mappings are to be placed in this map
+     */
+    public HashMap(Map<? extends K,? extends V> m){
+        if (m==null){
+            throw new NullPointerException("Argument cannot be null");
+        }
+        capacity=16;
+        this.loadFactor=0.75F;
+        buckets=new List[capacity];
+        for (int i=0;i<capacity;i++){
+            buckets[i]=new ArrayList<>();
+        }
+        sizeHashMap=0;
+    }
+
+    /**
+     * Returns the number of key-value mappings in this map.
+     *
+     * @return the number of key-value mappings in this map
+     */
+    @Override
+    public int size(){
+        return sizeHashMap;
+    }
+
+    /**
+     * Returns true if this map contains no key-value mappings.
+     *
+     * @return true if this map contains no key-value mappings
+     */
+    @Override
+    public boolean isEmpty(){
+        return size()==0;
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the value to which the specified key is mapped, or null if this map contains no mapping for the key
+     */
+    @Override
+    public V get(Object key){
+        for (int i=0;i<capacity;i++){
+            for (EntryHashMap<K,V> e : buckets[i]){
+                if (key.equals(e.getKey())){
+                    return e.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns true if this map contains a mapping for the specified key.
+     *
+     * @param key key whose presence in this map is to be tested
+     * @return true if this map contains a mapping for the specified key.
+     */
+    @Override
+    public boolean containsKey(Object key){
+        for (int i=0;i<capacity;i++){
+            for (EntryHashMap<K,V> e : buckets[i]){
+                if (key.equals(e.getKey())){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Associates the specified value with the specified key in this map.
+     * If the map previously contained a mapping for the key, the old value is replaced.
+     *
+     * @param key key with which the specified value is to be associated
+     * @param value value to be associated with the specified key
+     * @return the previous value associated with key, or null if there was no mapping for key
+     */
+    @Override
+    public V put(K key, V value){
+        int index=key.hashCode() % capacity;
+        V old_value=null;
+        for (EntryHashMap<K,V> e : buckets[index]){
+            if (key.equals(e.getKey())){
+                old_value=e.getValue();
+                e.setValue(value);
+                return old_value;
+            }
+        }
+        buckets[index].add(new EntryHashMap<>(key,value));
+        return old_value;
+    }
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return Set.of();
+    }
+}
--- memory/src/test/java/fr/ufrst/m1info/gl/compGL/HashMapTest.java
+++ memory/src/test/java/fr/ufrst/m1info/gl/compGL/HashMapTest.java
@@ -4,10 +4,12 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.Optional;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class HashMapTest
     extends TestCase
 {
     /**
@@ -15,7 +17,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public HashMapTest(String testName )
     {
         super( testName );
     }
@@ -25,14 +27,15 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( HashMapTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+
     public void testApp()
     {
-        assertTrue( true );
+
+        HashMap<Integer, Integer> map=new HashMap<Integer, Integer>();
+        map.put(4,81);
+        assertEquals(Optional.of(81),Optional.of(map.get(4)));
     }
 }
