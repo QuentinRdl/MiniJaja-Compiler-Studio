6fa82a2143cfe48edb32c4eeca5c8de5a7b92ad7
QuentinRdl <quentinradlo.pro@gmail.com>
2025-10-11 18:37:25 +0200

    [ADD] - JavaDoc for the Stack_Variable class

--- memory/src/main/java/fr/ufrst/m1info/gl/compGL/Stack_Variable.java
+++ memory/src/main/java/fr/ufrst/m1info/gl/compGL/Stack_Variable.java
@@ -3,7 +3,6 @@ package fr.ufrst.m1info.gl.compGL;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-// TODO : Write the JavaDoc
 // TODO : Do the integration tests w/ the Stack class
 
 /**
@@ -15,28 +14,54 @@ public class Stack_Variable {
     private Object value;
     private final int scope;
 
+    /**
+     * Constructor
+     * @param name name of the var
+     * @param value value of the var
+     * @param scope scope of the var
+     */
     public Stack_Variable(String name, Object value, int scope) {
         this.name = name;
         this.value = value;
         this.scope = scope;
     }
 
+    /**
+     * Returns the name of the var
+     * @return String name of the var
+     */
     public String getName() {
        return name;
     }
 
+    /**
+     * Returns the value of the var
+     * @return Object value of the var
+     */
     public Object getValue() {
         return value;
     }
 
+    /**
+     * Returns the scope of the var
+     * @return int Scope of the var
+     */
     public int getScope() {
         return scope;
     }
 
+    /**
+     * Sets the value of the var
+     * @param value new value for the var
+     */
     public void setValue(Object value) {
         this.value = value;
     }
 
+    /**
+     * Prints the var
+     * @return String the var
+     */
     @Override
     public String toString() {
         return name + "_" + scope + "=" + value;
