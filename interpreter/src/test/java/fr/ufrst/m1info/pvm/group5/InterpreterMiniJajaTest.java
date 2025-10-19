a5c2ec98a762674347ab2c64f28c8a1c2e3f9319
theo humbert <theo.humbert06@gmail.com>
2025-10-19 15:51:06 +0200

    [TEST] Add tests for InterpreterMiniJaja

--- interpreter/src/test/java/fr/ufrst/m1info/pvm/group5/InterpreterMiniJajaTest.java
+++ interpreter/src/test/java/fr/ufrst/m1info/pvm/group5/InterpreterMiniJajaTest.java
@@ -50,78 +50,42 @@ public class InterpreterMiniJajaTest {
     @Test
     @DisplayName("Interpret Undefined Variable / sum")
     public void UndefinedVariableSum() {
+        String errMessage=imj.interpretCode("class C {int y = 10;main {x += y;}}");
+        Assertions.assertNotEquals(null,errMessage);
+        Assertions.assertEquals(ASTInvalidMemoryException.class.toString(),errMessage.split(":")[0].trim());
+    }
+
+    @Test
+    @DisplayName("Interpret Undefined Variable / Inc")
+    public void UndefinedVariableInc()  {
         String errMessage=imj.interpretCode("class C {int y = 10;main {x++;}}");
         Assertions.assertNotEquals(null,errMessage);
         Assertions.assertEquals(ASTInvalidMemoryException.class.toString(),errMessage.split(":")[0].trim());
     }
-/*
+
     @Test
-    @DisplayName("Evaluation - Undefined Variable / Inc")
-    public void UndefinedVariableInc() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromString("class C {int y = 10;main {x++}}");
-        boolean success = false;
-        try {
-            AST.interpret(MemoryMock);
-        }
-        catch (ASTInvalidMemoryException e) {
-            success = true;
-        }
-        assertTrue(success);
+    @DisplayName("Interpret Undefined Variable / Evaluation")
+    public void UndefinedVariableEval()  {
+        String errMessage=imj.interpretCode("class C {int y = 10;main {y = x;}}");
+        Assertions.assertNotEquals(null,errMessage);
+        Assertions.assertEquals(ASTInvalidMemoryException.class.toString(),errMessage.split(":")[0].trim());
+    }
+
+    /*@Test
+    @DisplayName("Interpret Local Variables")
+    public void LocalVariables()  {
+        Assertions.assertNull(imj.interpretFile("src/test/resources/LocalVariables.mjj"));
     }
 
     @Test
-    @DisplayName("Evaluation - Undefined Variable / Evaluation")
-    public void UndefinedVariableEval() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromString("class C {int y = 10;main {y = x;}}");
-        boolean success = false;
-        try {
-            AST.interpret(MemoryMock);
-        }
-        catch (ASTInvalidMemoryException e) {
-            success = true;
-        }
-        assertTrue(success);
+    @DisplayName("Interpret Conditionnals")
+    public void Conditionnals()  {
+        Assertions.assertNull(imj.interpretFile("src/test/resources/Conditionals.mjj"));
     }
 
     @Test
-    @DisplayName("Evaluation - Local Variables")
-    public void LocalVariables() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile("src/test/resources/LocalVariables.mjj");
-        try {
-            AST.interpret(MemoryMock);
-        }
-        catch (Exception e) {
-            fail(e.getMessage());
-        }
-        assertEquals(9, memoryStorage.get("x").valueInt);
-    }
-
-    @Test
-    @DisplayName("Evaluation - Conditionnals")
-    public void Conditionnals() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile("src/test/resources/Conditionals.mjj");
-        try {
-            AST.interpret(MemoryMock);
-        }
-        catch (Exception e) {
-            fail(e.getMessage());
-        }
-        assertEquals(5, memoryStorage.get("a").valueInt);
-        assertEquals(6, memoryStorage.get("b").valueInt);
-        assertEquals(5, memoryStorage.get("c").valueInt);
-        assertEquals(6, memoryStorage.get("v").valueInt);
-    }
-
-    @Test
-    @DisplayName("Evaluation - Loops")
-    public void Loops() throws Exception {
-        AbstractSyntaxTree AST = AbstractSyntaxTree.fromFile("src/test/resources/Loops.mjj");
-        try {
-            AST.interpret(MemoryMock);
-        }
-        catch (Exception e) {
-            fail(e.getMessage());
-        }
-        assertEquals(104, memoryStorage.get("x").valueInt);
+    @DisplayName("Interpret Loops")
+    public void Loops()  {
+        Assertions.assertNull(imj.interpretFile("src/test/resources/Loops.mjj"));
     }*/
 }
