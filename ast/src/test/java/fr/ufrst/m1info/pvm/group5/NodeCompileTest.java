7d93366a17270bf6a2618e8a7493347439f1b8e2
Laura <laura.m.jacqueson@edu.univ-fcomte.fr>
2025-10-23 23:42:13 +0200

    [TEST] Added tests for compiling binary operations

---
+++ ast/src/test/java/fr/ufrst/m1info/pvm/group5/NodeCompileTest.java
@@ -0,0 +1,80 @@
+package fr.ufrst.m1info.pvm.group5;
+
+import org.junit.jupiter.api.*;
+
+import fr.ufrst.m1info.pvm.group5.Nodes.*;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+public class NodeCompileTest {
+    @Test
+    public void AddNode_Operation(){
+        NumberNode lop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        NumberNode rop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        AddNode tested = new AddNode(lop,rop);
+        assertEquals(List.of("push(5)","push(5)", "add"), tested.compile(1));
+    }
+
+    @Test
+    public void BinMinusNode_Operation(){
+        NumberNode lop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        NumberNode rop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        BinMinusNode tested = new BinMinusNode(lop,rop);
+        assertEquals(List.of("push(5)","push(5)", "minus"), tested.compile(1));
+    }
+
+    @Test
+    public void AndNode_Operation(){
+        NumberNode lop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        NumberNode rop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        AndNode tested = new AndNode(lop,rop);
+        assertEquals(List.of("push(5)","push(5)", "and"), tested.compile(1));
+    }
+
+    @Test
+    public void DivNode_Operation(){
+        NumberNode lop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        NumberNode rop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        DivNode tested = new DivNode(lop,rop);
+        assertEquals(List.of("push(5)","push(5)", "div"), tested.compile(1));
+    }
+
+    @Test
+    public void EqualNode_Operation(){
+        NumberNode lop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        NumberNode rop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        EqualNode tested = new EqualNode(lop,rop);
+        assertEquals(List.of("push(5)","push(5)", "cmp"), tested.compile(1));
+    }
+
+    @Test
+    public void MulNode_Operation(){
+        NumberNode lop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        NumberNode rop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        MulNode tested = new MulNode(lop,rop);
+        assertEquals(List.of("push(5)","push(5)", "mul"), tested.compile(1));
+    }
+
+    @Test
+    public void OrNode_Operation(){
+        NumberNode lop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        NumberNode rop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        OrNode tested = new OrNode(lop,rop);
+        assertEquals(List.of("push(5)","push(5)", "or"), tested.compile(1));
+    }
+
+    @Test
+    public void SupNode_Operation(){
+        NumberNode lop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        NumberNode rop = ASTMocks.createNode(NumberNode.class, null,i -> List.of("push(5)"));
+        SupNode tested = new SupNode(lop,rop);
+        assertEquals(List.of("push(5)","push(5)", "sup"), tested.compile(1));
+    }
+
+    
+
+
+}
