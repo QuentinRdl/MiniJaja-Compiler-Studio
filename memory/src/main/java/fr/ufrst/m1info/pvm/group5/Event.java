f706029e36fb83beb4018bcdbc47f69d7ec435d0
Unity <k.jelic.74@gmail.com>
2025-10-25 21:17:10 +0200

    [ADD] Added the event class, to do properly do publish-subscribe pattern

---
+++ memory/src/main/java/fr/ufrst/m1info/pvm/group5/Event.java
@@ -0,0 +1,56 @@
+package fr.ufrst.m1info.pvm.group5;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.function.Consumer;
+
+/**
+ * Simple implementation of an event class of the publish-subscribe pattern
+ * An event is a publisher class. Other classes can subscribe to an event, and will be notified when the event is triggered.
+ * The class the event is part of will be in charge of triggering the event
+ * @param <T>
+ */
+public class Event<T> {
+    /**
+     * Consumers subscribed to the event
+     */
+    private ConcurrentLinkedQueue<Consumer<T>> _subscribers;
+
+    /**
+     * Subscribe to the event. The function will be called when the event is triggered
+     * @param subscriber function to add to the event
+     */
+    public void subscribe(Consumer<T> subscriber) {
+        _subscribers.add(subscriber);
+    }
+
+    /**
+     * Remove a subscribed function from the event
+     * @param subscriber function to remove
+     */
+    public void unsubscribe(Consumer<T> subscriber) {
+        _subscribers.remove(subscriber);
+    }
+
+    /**
+     * Trigger the event with the event data
+     * /!\ This function should not be called by subscribers
+     * @param eventData data of the event
+     */
+    public void Trigger(T eventData){
+        for(Consumer<T> c : _subscribers){
+            c.accept(eventData);
+        }
+    }
+
+    /**
+     * Asynchronously Trigger the event with the event data
+     * /!\ This function should not be called by subscribers
+     * @param eventData data of the event
+     */
+    public void TriggerAsync(T eventData){
+        for(Consumer<T> c : _subscribers){
+            CompletableFuture.runAsync(() -> c.accept(eventData));
+        }
+    }
+}
