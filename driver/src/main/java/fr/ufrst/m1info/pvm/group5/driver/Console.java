ae28a809517cf71e5c645e45122298c3b33ae1ba
Unity <k.jelic.74@gmail.com>
2025-10-25 23:34:28 +0200

    [ADD] Implementation of the console class

--- driver/src/main/java/fr/ufrst/m1info/pvm/group5/driver/Console.java
+++ driver/src/main/java/fr/ufrst/m1info/pvm/group5/driver/Console.java
@@ -4,6 +4,27 @@ import fr.ufrst.m1info.pvm.group5.memory.Writer;
 import javafx.scene.control.TextArea;
 
 public class Console {
-    TextArea textArea;
-    Writer writer;
+    /**
+     * Textarea to add to the window
+     */
+    public TextArea textArea;
+    /**
+     * Writer associated to the textarea. Anything written to the writer will be added to the textarea.
+     */
+    private final Writer writer;
+
+    public Console(){
+        textArea = new TextArea();
+        textArea.setEditable(false);
+        writer = new Writer();
+        writer.TextAddedEvent.subscribe(e -> textArea.appendText(e.diff()));
+    }
+
+    /**
+     * Get a reference to the writer used by the console to be used somewhere else
+     * @return writer used by the console.
+     */
+    public Writer getWriter() {
+        return writer;
+    }
 }
