482b5ce688af7a451e99cfa65d6f7bfcfedd9c38
Unity <k.jelic.74@gmail.com>
2025-11-09 17:56:04 +0100

    [ADD] Created the classes and package related to the heap

---
+++ memory/src/main/java/fr/ufrst/m1info/pvm/group5/memory/Heap/Heap.java
@@ -0,0 +1,7 @@
+package fr.ufrst.m1info.pvm.group5.memory.Heap;
+
+/**
+ * Implementation of the heap
+ */
+public class Heap {
+}
---
+++ memory/src/main/java/fr/ufrst/m1info/pvm/group5/memory/Heap/HeapElement.java
@@ -0,0 +1,7 @@
+package fr.ufrst.m1info.pvm.group5.memory.Heap;
+
+/**
+ * Elements used for internal storage of the heap
+ */
+public class HeapElement {
+}
