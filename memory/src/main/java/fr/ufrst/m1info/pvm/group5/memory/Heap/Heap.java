36a989609e929fe0ba38575ad83f1b42c8c94956
Unity <k.jelic.74@gmail.com>
2025-11-10 17:57:59 +0100

    [ADD] Added the header of the base methods of the heap

--- memory/src/main/java/fr/ufrst/m1info/pvm/group5/memory/Heap/Heap.java
+++ memory/src/main/java/fr/ufrst/m1info/pvm/group5/memory/Heap/Heap.java
@@ -1,7 +1,112 @@
 package fr.ufrst.m1info.pvm.group5.memory.Heap;
 
+import fr.ufrst.m1info.pvm.group5.memory.SymbolTable.DataType;
+import fr.ufrst.m1info.pvm.group5.memory.Value;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Implementation of the heap
  */
 public class Heap {
+    final int totalSize;
+    int availableSize;
+    HeapElement currentElement;
+    byte[] storage;
+    Map<Integer, HeapElement> addresses;
+
+    /**
+     * Creates a heap of the given size
+     * @param totalSize total size of the heap
+     */
+    public Heap(int totalSize) {
+        this.totalSize = totalSize;
+        this.availableSize = totalSize;
+        this.storage = new byte[totalSize];
+        addresses = new HashMap<>();
+    }
+
+    /**
+     * Creates a heap. The default size of the heap is 2Ko.
+     */
+    public Heap(){
+        this(2048);
+    }
+
+    private int lastExternalAddress = -1;
+    /**
+     * Returns an int to create a new external address
+     * @return new external address created
+     */
+    private int newExtAddress(){
+        lastExternalAddress++;
+        return lastExternalAddress;
+    }
+
+    /**
+     * Allocate a space in the heap
+     * @param size size of the space to allocate
+     * @param type type of value stored in the memory space
+     * @return address of the created memory space
+     * @throws InsufficientMemoryException throws an exception if the given size is superior to the available one
+     */
+    public int allocate(int size, DataType type) throws InsufficientMemoryException{
+        return 0;
+    }
+
+    /**
+     * Remove a memory space from the heap
+     * @param address address to the element in the heap
+     * @throws InsufficientMemoryException throws an exception if the address is not referenced in the heap
+     */
+    public void free(int address) throws InsufficientMemoryException{
+
+    }
+
+    /**
+     * Notify the garbage collector a new reference has been added to a memory space
+     * @param address address of an element in the heap
+     * @throws InvalidMemoryAddressException throws an exception if the address is not referenced in the heap
+     */
+    public void addReference(int address) throws InvalidMemoryAddressException{
+    }
+
+    /**
+     * Notify the garbage collector a reference has been removed from a memory space
+     * @param address address of an element in the heap
+     * @throws InvalidMemoryAddressException throws an exception if the address is not referenced in the heap
+     */
+    public void removeReference(int address) throws InvalidMemoryAddressException{
+    }
+
+    /**
+     * Get the value of an element at a given address
+     * @param address address located within the heap
+     * @return value stored at the given address
+     * @throws InvalidMemoryAddressException throws an exception if the address is not referenced in the heap
+     */
+    public Value getValue(int address) throws InvalidMemoryAddressException{
+        return null;
+    }
+
+    /**
+     * Get a value of an element given by the starting address of an object and an offset
+     * @param address address of a block of the heap
+     * @param offset offset of the element
+     * @return value stored at the given address
+     * @throws InvalidMemoryAddressException throws an exception if the address is not referenced in the heap
+     */
+    public Value getValue(int address, int offset) throws InvalidMemoryAddressException{
+        return null;
+    }
+
+    /**
+     * Relocate every allocated element of the heap at the start of the array, and merge the remaining ones at the end.
+     * This allows to minimize the internal fragmentation of the heap.
+     * This operation is extremely expensive, and should only be called if necessary
+     */
+    private void defragment(){
+
+    }
 }
--- memory/src/main/java/fr/ufrst/m1info/pvm/group5/memory/Heap/HeapElement.java
+++ memory/src/main/java/fr/ufrst/m1info/pvm/group5/memory/Heap/HeapElement.java
@@ -1,7 +1,80 @@
 package fr.ufrst.m1info.pvm.group5.memory.Heap;
 
+import fr.ufrst.m1info.pvm.group5.memory.SymbolTable.DataType;
+
 /**
- * Elements used for internal storage of the heap
+ * Storage block of the heap
  */
 public class HeapElement {
+    public int internalAddress;
+    public int externalAddress;
+    public int size;
+    public DataType storageType;
+    public boolean isFree;
+    public int references;
+    HeapElement prev;
+    HeapElement next;
+
+    public class InsufficientSizeException extends Exception {
+        InsufficientSizeException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Creates a new heap element with no adjacent element
+     * @param internalAddress internal address of the storage referenced by the element
+     * @param externalAddress external address of the element stored
+     * @param size size of the block to create
+     * @param storageType data stored in the element
+     */
+    public HeapElement(int internalAddress, int externalAddress, int size, DataType storageType) {}
+
+    /**
+     * Tries to merge the element with an adjacent one.
+     * If one of the elements is not free, the merge will fail.
+     * @param other other element to merge with this one.
+     */
+    private void TryMerge(HeapElement other){}
+
+    /**
+     * Tries to merge the element with every adjacent element.
+     * If this element or it's adjacent ones are not free, the merge will fail.
+     */
+    public void TryMerge(){
+        TryMerge(prev);
+        TryMerge(next);
+    }
+
+    /**
+     * Creates a new heap element by reducing the size of this one.
+     * By default, the new element will be placed after this one.
+     * @param size size of the new element to create
+     * @return newly created element
+     * @throws InsufficientSizeException throws an exception if the requested size is bigger than the size of the element
+     */
+    public HeapElement split(int size) throws InsufficientSizeException {
+        return null;
+    }
+
+    /**
+     * Creates a new heap element by reducing the size of this one.
+     * By default, the new element will be placed after this one.
+     * @param size size of the new element to create
+     * @param before if true, the new element will be placed before this one instead of after
+     * @return newly created element
+     * @throws InsufficientSizeException throws an exception if the requested size is bigger than the size of the element
+     */
+    public HeapElement split(int size, boolean before) throws InsufficientSizeException {
+        return null;
+    }
+
+    /**
+     * Tests if an address is located within the block
+     * @param internalAddress address to compare
+     * @return true if the address is located within this element, false otherwise
+     */
+    public boolean belongsTo(int internalAddress){
+        return false;
+    }
 }
