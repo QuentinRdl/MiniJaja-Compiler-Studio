3fe66c2098fcd1655e4689c57d5e72aaa3ee1d4f
jmagni10 <julie.magnin03@edu.univ-fcomte.fr>
2025-11-12 19:35:37 +0100

    [ADD] Added a few JajaCode instructions (nop, goto, swap, pop)

--- ast/src/test/java/fr/ufrst/m1info/pvm/group5/ast/InstructionsUnitTest.java
+++ ast/src/test/java/fr/ufrst/m1info/pvm/group5/ast/InstructionsUnitTest.java
@@ -1,9 +1,10 @@
 package fr.ufrst.m1info.pvm.group5.ast;
 
-import fr.ufrst.m1info.pvm.group5.ast.Instructions.PushInstruction;
+import fr.ufrst.m1info.pvm.group5.ast.Instructions.*;
 import fr.ufrst.m1info.pvm.group5.memory.Memory;
 import fr.ufrst.m1info.pvm.group5.memory.Value;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
@@ -44,5 +45,51 @@ public class InstructionsUnitTest {
         assertEquals(0, storage.pop().second().valueInt);
     }
 
-    //
+    @Disabled
+    @Test
+    public void pop_simple() throws Exception{
+        PushInstruction p1 = new PushInstruction(new Value(5));
+        var res = p1.execute(0, memory);
+        assertEquals(1, res);
+        PopInstruction p2 = new PopInstruction();
+        res = p2.execute(1, memory);
+        assertEquals(2, res);
+        assertTrue(storage.isEmpty());
+    }
+
+    //TODO: empty pop test
+
+    @Test
+    public void swap_simple_instruction() throws Exception {
+        PushInstruction p1 = new PushInstruction(new Value(2));
+        PushInstruction p2 = new PushInstruction(new Value(5));
+
+        p1.execute(0, memory);
+        p2.execute(1, memory);
+
+        SwapInstruction swap = new SwapInstruction();
+        var res = swap.execute(2, memory);
+
+        assertEquals(3, res);
+        assertEquals(2, storage.pop().second().valueInt);
+        assertEquals(5, storage.pop().second().valueInt);
+    }
+
+    //TODO: empty swap test
+
+    @Test
+    public void goto_instruction() throws Exception {
+        GotoInstruction i = new GotoInstruction(5);
+        var res = i.execute(0, memory);
+        assertEquals(5, res);
+    }
+
+    @Test
+    public void nop_instruction() throws Exception {
+        NopInstruction i = new NopInstruction();
+        var res = i.execute(0, memory);
+        assertEquals(1, res);
+    }
+
+
 }
