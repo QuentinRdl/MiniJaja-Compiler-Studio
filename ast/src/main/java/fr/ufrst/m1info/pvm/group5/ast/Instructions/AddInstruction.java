e99664e86b3358b988ddf7864ffe3ca6a75963c3
Laura <laura.m.jacqueson@edu.univ-fcomte.fr>
2025-11-15 20:26:45 +0100

    [UPD] Added operand type checking

--- ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Instructions/AddInstruction.java
+++ ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Instructions/AddInstruction.java
@@ -1,5 +1,6 @@
 package fr.ufrst.m1info.pvm.group5.ast.Instructions;
 
+import fr.ufrst.m1info.pvm.group5.ast.ASTInvalidTypeException;
 import fr.ufrst.m1info.pvm.group5.memory.Memory;
 import fr.ufrst.m1info.pvm.group5.memory.SymbolTable.DataType;
 import fr.ufrst.m1info.pvm.group5.memory.SymbolTable.EntryKind;
@@ -11,6 +12,9 @@ public class AddInstruction extends Instruction{
     public int execute(int address, Memory m) {
         Value v2 = (Value) m.pop(); //first pop => right operand
         Value v1 = (Value) m.pop(); //second pop => left operand
+        if (v1.Type != ValueType.INT || v2.Type != ValueType.INT){
+            throw new ASTInvalidTypeException("Type error: add instruction expects two INT operands, but received " + v1.Type + " and " + v2.Type + ".");
+        }
         int res = v1.valueInt + v2.valueInt;
         Value vres = new Value(res);
         m.push(".", vres, DataType.INT, EntryKind.CONSTANT);
