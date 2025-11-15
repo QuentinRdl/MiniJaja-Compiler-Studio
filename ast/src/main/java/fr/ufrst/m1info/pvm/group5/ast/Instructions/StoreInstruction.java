0889074ffe38e52c248c71ea8470578f891d717e
Laura <laura.m.jacqueson@edu.univ-fcomte.fr>
2025-11-15 21:36:47 +0100

    [UPD] Added type checking for the parameter

--- ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Instructions/StoreInstruction.java
+++ ast/src/main/java/fr/ufrst/m1info/pvm/group5/ast/Instructions/StoreInstruction.java
@@ -1,6 +1,9 @@
 package fr.ufrst.m1info.pvm.group5.ast.Instructions;
 
+import fr.ufrst.m1info.pvm.group5.ast.ASTInvalidTypeException;
 import fr.ufrst.m1info.pvm.group5.memory.Memory;
+import fr.ufrst.m1info.pvm.group5.memory.Value;
+import fr.ufrst.m1info.pvm.group5.memory.ValueType;
 
 public class StoreInstruction extends Instruction{
     String ident;
@@ -11,7 +14,11 @@ public class StoreInstruction extends Instruction{
 
     @Override
     public int execute(int address, Memory m) {
-        m.affectValue(this.ident, m.pop());
+        Value v = (Value) m.pop();
+        if (v.Type == ValueType.STRING){
+            throw new ASTInvalidTypeException("Type error: operation does not accept STRING type, but received " + v.Type + ".");
+        }
+        m.affectValue(this.ident, v);
         return address+1;
     }
 }
