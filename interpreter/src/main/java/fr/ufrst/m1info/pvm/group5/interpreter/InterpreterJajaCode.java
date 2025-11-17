12e93f0298c15cfe38e229a7972109f65300ee2e
theo humbert <theo.humbert06@gmail.com>
2025-11-17 19:48:58 +0100

    [ADD] Add class InterpreterJajaCode

---
+++ interpreter/src/main/java/fr/ufrst/m1info/pvm/group5/interpreter/InterpreterJajaCode.java
@@ -0,0 +1,56 @@
+package fr.ufrst.m1info.pvm.group5.interpreter;
+
+
+import fr.ufrst.m1info.pvm.group5.memory.Memory;
+import fr.ufrst.m1info.pvm.group5.ast.AbstractSyntaxTree;
+import fr.ufrst.m1info.pvm.group5.memory.Writer;
+
+public class InterpreterJajaCode implements Interpreter{
+    Writer output;
+
+    protected InterpreterJajaCode(){
+        output = new Writer();
+    }
+
+    public InterpreterJajaCode(Writer output) {
+        this.output = output;
+    }
+
+    /**
+     * Interpret the given code
+     *
+     * @param code the code we want to interpret
+     * @return the error message
+     */
+    @Override
+    public String interpretCode(String code) {
+        Memory mem = new Memory(output);
+        String errMessage= null;
+        try{
+            AbstractSyntaxTree ast = AbstractSyntaxTree.fromString(code);
+            ast.interpret(mem);
+        } catch (Exception e) {
+            errMessage=e.getClass()+" : "+e.getMessage();
+        }
+        return errMessage;
+    }
+
+    /**
+     * Interpret the given file
+     *
+     * @param path the path of the file we want to interpret
+     * @return the error message
+     */
+    @Override
+    public String interpretFile(String path)  {
+        Memory mem = new Memory(output);
+        String errMessage= null;
+        try{
+            AbstractSyntaxTree ast = AbstractSyntaxTree.fromFile(path);
+            ast.interpret(mem);
+        } catch (Exception e) {
+            errMessage=e.getClass()+" : "+e.getMessage();
+        }
+        return errMessage;
+    }
+}
