73bc71314ef791516def09b3aa57c6041a06a91b
QuentinRdl <quentinradlo.pro@gmail.com>
2025-11-20 13:52:53 +0100

    [FIX] - Removed debug sout

--- memory/src/main/java/fr/ufrst/m1info/pvm/group5/memory/Memory.java
+++ memory/src/main/java/fr/ufrst/m1info/pvm/group5/memory/Memory.java
@@ -467,7 +467,6 @@ public class Memory {
         }
 
         int address = (int) addressObj.getValue();
-        System.out.println("The address is = " + address + " for identifier " + identifier + " and index " + index + " with a value = " + heap.getValue(address, index));
         return heap.getValue(address, index);
     }
 
