0d7058e47cf29251a005e7c5f9ffc448994ff602
Unity <k.jelic.74@gmail.com>
2025-11-25 22:40:03 +0100

    [ADD] Added new methods to the interpreter interface to allow to interpret in step by step or breakpoints

--- interpreter/src/main/java/fr/ufrst/m1info/pvm/group5/interpreter/Interpreter.java
+++ interpreter/src/main/java/fr/ufrst/m1info/pvm/group5/interpreter/Interpreter.java
@@ -1,6 +1,11 @@
 package fr.ufrst.m1info.pvm.group5.interpreter;
 
+import fr.ufrst.m1info.pvm.group5.ast.InterpretationMode;
+import fr.ufrst.m1info.pvm.group5.memory.Event;
+
 public interface Interpreter {
+    public Event<InterpretationHaltedData> interpretationHaltedEvent = new Event<>();
+
     /**
      * Interpret the given code
      *
@@ -10,10 +15,45 @@ public interface Interpreter {
     String interpretCode(String code);
 
     /**
+     * Interpret the given code in a given mode
+     *
+     * @param code the code we want to interpret
+     * @param mode interpretation mode
+     * @return error messages
+     */
+    String interpretCode(String code, InterpretationMode mode);
+
+    /**
      * Interpret the given file
      *
      * @param path the path of the file we want to interpret
      * @return the error message
      */
     String interpretFile(String path);
+
+    /**
+     * Interpret the given file in a given mode
+     *
+     * @param path path to the file we want to interpret
+     * @param mode interpretation mode
+     * @return error messages
+     */
+     String interpretFile(String path, InterpretationMode mode);
+
+    /**
+     * Stops the current interpretation
+     */
+    void stopInterpretation();
+
+    /**
+     * Resumes the current interpretation in the given mode
+     *
+     * @param mode interpretation mode to resume the intrepretation into
+     */
+    void resumeInterpretation(InterpretationMode mode);
+
+    /**
+     * Waits for the current interpretation to halt
+     */
+    void waitInterpretation();
 }
